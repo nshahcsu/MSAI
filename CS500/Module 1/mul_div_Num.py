dff6f8d6e51ee817bb3e88b68a271687cf4a540c
nshahcsu <neel.ddit@gmail.com>
2024-05-19 23:28:28 -0600

    Update mul_div_Num.py
    Part 2 - Multiply and Divide numbers

---
+++ CS500/Module 1/mul_div_Num.py
@@ -0,0 +1,15 @@
+# Write a Python program to find the multiplication and division of two numbers.
+
+# Ask the user to input two numbers (num1 and num2). 
+# Given those two numbers, multiply them together to find the output. 
+# Also, divide num1/num2 to find the output.
+
+# Get the user input 
+num1  = int(input('Enter 1st number: '))
+num2  = int(input('Enter 2nd number: '))
+
+# Prints Multiplication of two numbers
+print('The Multiplication of', num1 , 'and', num2, 'is', num1 *num2 )
+
+# Prints Divion of 1st number by 2nd Number
+print('The Division of', num1  ,'by', num2, 'is', num1 /num2 )
