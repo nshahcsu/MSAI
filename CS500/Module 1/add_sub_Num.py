2c3d4fe06ea22c039ce4c3f039eb22f17550be70
nshahcsu <neel.ddit@gmail.com>
2024-05-19 23:26:53 -0600

    Update add_sub_Num.py
    Part 1 - Add and Subtract numbers

---
+++ CS500/Module 1/add_sub_Num.py
@@ -0,0 +1,14 @@
+# Write a Python program to find the addition and subtraction of two numbers.
+
+# Ask the user to input two numbers (num1 and num2). 
+# Given those two numbers, add them together to find the output. Also, subtract the two numbers to find the output.
+
+# Get the user input 
+num1 = int(input('Enter 1st number: '))
+num2 = int(input('Enter 2nd number: '))
+
+# Prints sum of two numbers
+print('The Sum of', num1, 'and', num2, 'is', num1+num2 )
+
+# Prints substaction of 2nd number from the 1st number
+print('The difference of', num1 ,'and', num2, 'is', num1-num2 )
