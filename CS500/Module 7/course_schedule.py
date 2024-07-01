d1784a1765ae68fc4a504e07b03660a4a3c0ed84
nshahcsu <neel.ddit@gmail.com>
2024-06-30 20:29:21 -0600

    Module 7 Assignment

--- CS500/Module 7/course_schedule.py
+++ CS500/Module 7/course_schedule.py
@@ -26,14 +26,14 @@ MAX_FAIL_COUNT = 5
 fail_count = 0
 while (fail_count < MAX_FAIL_COUNT):
     try:
-        course_cd = input('Please enter Course Code to view schedule.\n').upper()
+        course_cd = input('Please enter a Course Number to view schedule.\n').upper()
         avail_courses = ''
         if course_cd in (dict_room_number and dict_instructors and dict_meeting):     
             print('     ___                            ___         __                         _    _      ')      
             print('    / __| ___  _  _  _ _  ___ ___  |_ _| _ _   / _| ___  _ _  _ __   __ _ | |_ (_) ___  _ _   ')
             print('   | (__ / _ \| || || \'_|(_-</ -_)  | | | \' \ |  _|/ _ \| \'_|| \'  \ / _\` ||  _|| |/ _ \| \' \ ')
             print('    \___|\___/ \_,_||_|  /__/\___| |___||_||_||_|  \___/|_|  |_|_|_|\__,_| \__||_|\___/|_||_| ')
-            print('\n\nInformation for the class:',course_cd)
+            print('\n\nInformation for the course:',course_cd)
             print("Room:", dict_room_number[course_cd], "Instructor:",dict_instructors[course_cd], "Meeting at:", dict_meeting[course_cd])
             if input("\n\nDo you wish to check details for another course? Type 'Y' for yes or press any key to Quit.\n").upper() == 'Y':
                 continue
@@ -46,7 +46,7 @@ while (fail_count < MAX_FAIL_COUNT):
                 print('\nMax allowed count of error reached. Please try again later.\n')
                 break
             else:
-                print("Please Enter correct Course Info.\nCurrently, Schedule is available for these courses:")
+                print("Course info not available for the course number you entered.\nCurrently, Schedule is available for these courses:")
                 for key in (dict_room_number and dict_instructors and dict_meeting):
                     avail_courses += (key + ' ')
                 print(avail_courses)
